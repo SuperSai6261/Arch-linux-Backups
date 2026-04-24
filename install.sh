987dc3c500e845c83ba67bd1846454892866a6b2
SuperSai6261 <saitanmayanayak2006@gmail.com>
2026-04-24 13:12:50 +0530

    feat: auto install zsh plugins in install.sh

--- install.sh
+++ install.sh
@@ -36,6 +36,15 @@ cp "$DOTFILES/zshrc" ~/.zshrc
 [ -f "$DOTFILES/.p10k.zsh" ] && cp "$DOTFILES/.p10k.zsh" ~/.p10k.zsh
 echo "✓ zsh"
 
+# Install zsh plugins
+if [ ! -d "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions" ]; then
+  git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+fi
+if [ ! -d "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting" ]; then
+  git clone https://github.com/zsh-users/zsh-syntax-highlighting ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+fi
+echo "✓ zsh plugins"
+
 echo ""
 echo "Done! Reboot or restart Hyprland to apply changes."
 echo "Neovim plugins will auto-install on first launch."
